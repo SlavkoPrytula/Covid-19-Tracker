6c7572105899d6546fd18f952f20bb2a84d907dd
Slavko Prytula <slavko.prytula@gmail.com>
2020-05-16 11:57:25 +0300

    test for COVID-19 ADT

---
+++ test_coronaADT.py
@@ -0,0 +1,41 @@
+from corona_adt import Corona
+import unittest
+
+
+class TestCoronaADT(unittest.TestCase):
+    def setUp(self) -> None:
+        self.country = "USA"
+        self.corona = Corona(self.country)
+
+    def test_APIs(self):
+        # test all the APIs:
+        self.assertIsInstance(self.corona.get_affected_countries(), dict)
+        self.assertIsInstance(self.corona.get_history_by_particular_country(self.country), dict)
+        self.assertIsInstance(self.corona.get_cases_by_country(), dict)
+        self.assertIsInstance(self.corona.get_latest_stat_by_country_name(self.country), dict)
+        self.assertIsInstance(self.corona.get_world_total_stat(), dict)
+
+    def test_all_cases(self):
+        # Get test results based on recorded information from beginning
+        self.assertIsInstance(self.corona.get_infected(), int)
+        self.assertIsInstance(self.corona.get_recovered(), int)
+        self.assertIsInstance(self.corona.get_deaths(), int)
+        try:
+            self.assertIsInstance(self.corona.get_total_cases_per1m(), float)
+        except AssertionError:
+            self.assertIsInstance(self.corona.get_total_cases_per1m(), int)
+
+    def test_day_cases(self):
+        # Get test results based on recorded information from the current day
+        self.assertIsInstance(self.corona.get_new_cases(), int)
+        self.assertIsInstance(self.corona.get_active_cases(), int)
+        self.assertIsInstance(self.corona.get_new_deaths(), int)
+        self.assertIsInstance(self.corona.get_serious_critical(), int)
+        self.assertIsInstance(self.corona.get_record_date(), str)
+
+    def test_interface(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
